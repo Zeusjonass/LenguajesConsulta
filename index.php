666336bec3bbf1e634e5569de31d8aff5d5b691d
Jef Peniche <jeff@nativodigital.com.mx>
2021-10-19 15:12:01 -0500

    Upload and save documents funcionality

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 include("./src/busqueda.php");
+include("./src/upload.php");
 ?>
 <html>
     <head>
@@ -14,12 +15,26 @@ include("./src/busqueda.php");
     </head>
     <body>
         <div id="container">
+            <h1>Subida de archivos</h1>
+            <form action="./index.php" method="post" enctype="multipart/form-data">
+                <label>Seleccione los archivos .txt que desee subir:</label>
+                <br>
+                <input name="upload[]" type="file" multiple="multiple"  accept=".txt">
+                <br><br>
+                <input type="submit" value="Subir">
+            </form>
+            <?php
+             if(isset( $_FILES['upload']['name'] )) {
+                subeArchivos();
+            }
+            ?>
+            <hr>
             <h1>Buscador Northwind</h1>
             <form action="./index.php" method="GET">
-                <label for="filtro">
+                <label>
                     Buscador:
                     <input id="busqueda" name="busqueda" type="text">
-                </label<br><br><br>
+                </label><br><br>
                 <input type="submit" value="Buscar">
             </form>
         </div> 
